e2ea3414f0ed405270d9777fd8512e4ac421a112
Jeff <jeffreyrjohnsonsemail@gmail.com>
2017-09-29 16:12:50 -0500

    Wiring Up has Many and Has One Relations

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-My files from coding along with Stephan Grinders Udemy course
-the-complete-developers-guide-to-mongodb
+My files from coding along with Stephan Grinder's Udemy course
+The complete developers guide to mongodb. None of this is my original code, but I sure did learn a lot from following this course, It very much drills in the concept of test drivin development.
--- test/association_test.js
+++ test/association_test.js
@@ -8,6 +8,11 @@ describe('Associations', () => {
 
     beforeEach((done) => {
         joe = new User({ name: 'Joe' });
-        blogPost = new BlogPost({ title: 'JS is Great', })
+        blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
+        comment = new Comment({ content: 'Congrats on great post' });
+
+        joe.blogPosts.push(blogPost);
+        blogPost.comments.push(comment);
+        comment.user = joe;
     });
 });
