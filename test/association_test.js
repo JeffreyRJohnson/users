7b32332e83f647fb9f4764e47bf090124c8cfb87
Jeff <jeffreyrjohnsonsemail@gmail.com>
2017-09-29 18:18:16 -0500

    Populating Queries

--- test/association_test.js
+++ test/association_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -21,8 +22,9 @@ describe('Associations', () => {
 
     it.only('saves a relation between a user and a blogpost', (done) => {
         User.findOne({ name: 'Joe' })
+            .populate('blogPosts')
             .then((user) => {
-                console.log(user);
+                assert(user.blogPosts[0].title === 'JS is Great');
                 done();
             });
     });
